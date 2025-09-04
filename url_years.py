bf8e53e30789b465e36227f79c5aa908f8e0b5fa
Jairo Mateus <jartmatm@gmail.com>
2025-09-04 11:37:01 +1000

    Add app.py, modelo_powerball.keras, url_years.py, and requirements.txt

---
+++ url_years.py
@@ -0,0 +1,6 @@
+urls = {}
+for year in range(2025, 1995, -1):
+    urls[year]=f'https://au.lottonumbers.com/powerball/results/{year}-archive'
+
+#for year, url in list(urls.items())[:]:  # solo los primeros 5
+#    print(year, url)
